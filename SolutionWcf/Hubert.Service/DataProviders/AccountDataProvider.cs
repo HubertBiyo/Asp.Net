7cecaf2cd6488809918c013b2ca101b7c3b3c6ea
HubertBiyo <15130079715@163.com>
2018-11-27 18:33:46 +0800

    wcfdemo

---
+++ SolutionWcf/Hubert.Service/DataProviders/AccountDataProvider.cs
@@ -0,0 +1,23 @@
+﻿using Hubert.Service.IDataProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubert.Service.DataProviders
+{
+    public class AccountDataProvider : IAccountDataProvider
+    {
+        public string GetStr()
+        {
+            return "Good Luck";
+        }
+
+        /// <summary>
+        /// 单例
+        /// </summary>
+        public AccountDataProvider() { }
+        public static AccountDataProvider Instance { get; } = new AccountDataProvider();
+    }
+}
